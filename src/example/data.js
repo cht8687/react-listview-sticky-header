7c9a0ca6b2393756f72cbf77e9fd2589b9269785
Haotian.Chang <cht8687@gmail.com>
2015-12-01 23:15:53 +1100

    wrap DATA in data.js

---
+++ src/example/data.js
@@ -0,0 +1,111 @@
+export const DATA = [
+  {
+    headerName : "ListA",
+    items : [{
+      title : "items1"
+    }, {
+      title : "items2"
+    }, {
+      title : "items3"
+    },{
+      title : "items4"
+    },{
+      title : "items5"
+    },{
+      title : "items6"
+    }]
+  }, {
+    headerName : "ListB",
+    items : [{
+      title : "items1"
+    }, {
+      title : "items2"
+    }, {
+      title : "items3"
+    },{
+      title : "items4"
+    },{
+      title : "items5"
+    },{
+      title : "items6"
+    }]
+  },{
+    headerName : "ListC",
+    items : [{
+      title : "items1"
+    }, {
+      title : "items2"
+    }, {
+      title : "items3"
+    },{
+      title : "items4"
+    },{
+      title : "items5"
+    },{
+      title : "items6"
+    }]
+  },{
+    headerName : "ListD",
+    items : [{
+      title : "items1"
+    }, {
+      title : "items2"
+    }, {
+      title : "items3"
+    },{
+      title : "items4"
+    },{
+      title : "items5"
+    },{
+      title : "items6"
+    }]
+  },{
+    headerName : "ListE",
+    items : [{
+      title : "items1"
+    }, {
+      title : "items2"
+    }, {
+      title : "items3"
+    },{
+      title : "items4"
+    },{
+      title : "items5"
+    },{
+      title : "items6"
+    }]
+  },{
+    headerName : "ListF",
+    items : [{
+      title : "items1"
+    }, {
+      title : "items2"
+    }, {
+      title : "items3"
+    }]
+  },{
+    headerName : "ListG",
+    items : [{
+      title : "items1"
+    }, {
+      title : "items2"
+    }, {
+      title : "items3"
+    },{
+      title : "items4"
+    },{
+      title : "items5"
+    },{
+      title : "items6"
+    }]
+  },{
+    headerName : "ListH",
+    items : [{
+      title : "items1"
+    }, {
+      title : "items2"
+    }, {
+      title : "items3"
+    }]
+  }
+];
