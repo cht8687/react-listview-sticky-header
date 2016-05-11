65bec8adca41f18331b8342b33e6aeb5f5ea3a00
Haotian Chang <cht8687@gmail.com>
2016-05-12 00:00:40 +1000

    add in test for listheader and listitem class

--- test/index.js
+++ test/index.js
@@ -17,11 +17,38 @@ test('----- React Component Tests: ReactListView -----', t => {
 })
 
 test('----- React Component Tests: ListHeader -----', t => {
+  t.plan(2)
   t.ok(ListHeader instanceof Function, 'should be function')
-  t.end()
+  const listHeader = {
+    width: '383px',
+    height: '20px',
+    background: 'green',
+    color: 'white'
+  }
+  const listheader = shallow(<ListHeader ref='ListHeader-0' header='listA' styles={listHeader} />)
+  t.equal(true, listheader.containsMatchingElement([<div>ListA</div>]))
 })
 
 test('----- React Component Tests: ListItems -----', t => {
+  t.plan(2)
   t.ok(ListItems instanceof Function, 'should be function')
+  const items = [{
+    title: 'items1'
+  }, {
+    title: 'items2'
+  }, {
+    title: 'items3'
+  }, {
+    title: 'items4'
+  }, {
+    title: 'items5'
+  }, {
+    title: 'items6'
+  }]
+  const listItems = {
+    color: 'blue'
+  }
+  const listitems = shallow(<ListItems items={items} styles={listItems} />)
+  t.equal(true, listitems.containsMatchingElement([<span>items1</span>]))
   t.end()
 })
