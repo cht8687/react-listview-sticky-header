cbb1cd8b15a0c33a05686a5954b4428d06a013ee
Robert Chang <cht8687@gmail.com>
2020-02-29 21:50:29 +1100

    fix lint

--- src/example/data.js
+++ src/example/data.js
@@ -4,314 +4,314 @@ export const DATA = [
     items: [{
       title: 'Macbook'
     },
-      {
-        title: 'Dell'
-      },
-      {
-        title: 'HP'
-      },
-      {
-        title: 'Asus'
-      },
-      {
-        title: 'SAMSUNG'
-      },
-      {
-        title: 'Office Works'
-      }]
+    {
+      title: 'Dell'
+    },
+    {
+      title: 'HP'
+    },
+    {
+      title: 'Asus'
+    },
+    {
+      title: 'SAMSUNG'
+    },
+    {
+      title: 'Office Works'
+    }]
   },
   {
     headerName: 'Home Appliances',
     items: [{
       title: 'LG'
     },
-      {
-        title: 'Philips'
-      },
-      {
-        title: 'BEKO'
-      },
-      {
-        title: 'Breville'
-      },
-      {
-        title: 'Fisher & Paykel'
-      },
-      {
-        title: 'Hisense'
-      }]
+    {
+      title: 'Philips'
+    },
+    {
+      title: 'BEKO'
+    },
+    {
+      title: 'Breville'
+    },
+    {
+      title: 'Fisher & Paykel'
+    },
+    {
+      title: 'Hisense'
+    }]
   },
   {
     headerName: 'Headphones & DJ',
     items: [{
       title: 'BOSE'
     },
-      {
-        title: 'Beats'
-      },
-      {
-        title: 'Sony'
-      },
-      {
-        title: 'JBL'
-      },
-      {
-        title: 'Marley'
-      },
-      {
-        title: 'Pioneer DJ'
-      }]
+    {
+      title: 'Beats'
+    },
+    {
+      title: 'Sony'
+    },
+    {
+      title: 'JBL'
+    },
+    {
+      title: 'Marley'
+    },
+    {
+      title: 'Pioneer DJ'
+    }]
   },
   {
     headerName: 'Kitchen',
     items: [{
       title: 'items1'
     },
-      {
-        title: 'items2'
-      },
-      {
-        title: 'items3'
-      },
-      {
-        title: 'items4'
-      },
-      {
-        title: 'items5'
-      },
-      {
-        title: 'items6'
-      }]
+    {
+      title: 'items2'
+    },
+    {
+      title: 'items3'
+    },
+    {
+      title: 'items4'
+    },
+    {
+      title: 'items5'
+    },
+    {
+      title: 'items6'
+    }]
   },
   {
     headerName: 'ListE',
     items: [{
       title: 'items1'
     },
-      {
-        title: 'items2'
-      },
-      {
-        title: 'items3'
-      },
-      {
-        title: 'items4'
-      },
-      {
-        title: 'items5'
-      },
-      {
-        title: 'items6'
-      }]
+    {
+      title: 'items2'
+    },
+    {
+      title: 'items3'
+    },
+    {
+      title: 'items4'
+    },
+    {
+      title: 'items5'
+    },
+    {
+      title: 'items6'
+    }]
   },
   {
     headerName: 'ListF',
     items: [{
       title: 'items1'
     },
-      {
-        title: 'items2'
-      },
-      {
-        title: 'items3'
-      },
-      {
-        title: 'items4'
-      },
-      {
-        title: 'items5'
-      },
-      {
-        title: 'items6'
-      }]
+    {
+      title: 'items2'
+    },
+    {
+      title: 'items3'
+    },
+    {
+      title: 'items4'
+    },
+    {
+      title: 'items5'
+    },
+    {
+      title: 'items6'
+    }]
   },
   {
     headerName: 'ListG',
     items: [{
       title: 'items1'
     },
-      {
-        title: 'items2'
-      },
-      {
-        title: 'items3'
-      },
-      {
-        title: 'items4'
-      },
-      {
-        title: 'items5'
-      },
-      {
-        title: 'items6'
-      }]
+    {
+      title: 'items2'
+    },
+    {
+      title: 'items3'
+    },
+    {
+      title: 'items4'
+    },
+    {
+      title: 'items5'
+    },
+    {
+      title: 'items6'
+    }]
   },
   {
     headerName: 'ListH',
     items: [{
       title: 'items1'
     },
-      {
-        title: 'items2'
-      },
-      {
-        title: 'items3'
-      },
-      {
-        title: 'items4'
-      },
-      {
-        title: 'items5'
-      },
-      {
-        title: 'items6'
-      }]
+    {
+      title: 'items2'
+    },
+    {
+      title: 'items3'
+    },
+    {
+      title: 'items4'
+    },
+    {
+      title: 'items5'
+    },
+    {
+      title: 'items6'
+    }]
   },
   {
     headerName: 'ListI',
     items: [{
       title: 'items1'
     },
-      {
-        title: 'items2'
-      },
-      {
-        title: 'items3'
-      },
-      {
-        title: 'items4'
-      },
-      {
-        title: 'items5'
-      },
-      {
-        title: 'items6'
-      }]
+    {
+      title: 'items2'
+    },
+    {
+      title: 'items3'
+    },
+    {
+      title: 'items4'
+    },
+    {
+      title: 'items5'
+    },
+    {
+      title: 'items6'
+    }]
   },
   {
     headerName: 'ListJ',
     items: [{
       title: 'items1'
     },
-      {
-        title: 'items2'
-      },
-      {
-        title: 'items3'
-      },
-      {
-        title: 'items4'
-      },
-      {
-        title: 'items5'
-      },
-      {
-        title: 'items6'
-      }]
+    {
+      title: 'items2'
+    },
+    {
+      title: 'items3'
+    },
+    {
+      title: 'items4'
+    },
+    {
+      title: 'items5'
+    },
+    {
+      title: 'items6'
+    }]
   },
   {
     headerName: 'ListK',
     items: [{
       title: 'items1'
     },
-      {
-        title: 'items2'
-      },
-      {
-        title: 'items3'
-      },
-      {
-        title: 'items4'
-      },
-      {
-        title: 'items5'
-      },
-      {
-        title: 'items6'
-      }]
+    {
+      title: 'items2'
+    },
+    {
+      title: 'items3'
+    },
+    {
+      title: 'items4'
+    },
+    {
+      title: 'items5'
+    },
+    {
+      title: 'items6'
+    }]
   },
   {
     headerName: 'ListL',
     items: [{
       title: 'items1'
     },
-      {
-        title: 'items2'
-      },
-      {
-        title: 'items3'
-      },
-      {
-        title: 'items4'
-      },
-      {
-        title: 'items5'
-      },
-      {
-        title: 'items6'
-      }]
+    {
+      title: 'items2'
+    },
+    {
+      title: 'items3'
+    },
+    {
+      title: 'items4'
+    },
+    {
+      title: 'items5'
+    },
+    {
+      title: 'items6'
+    }]
   },
   {
     headerName: 'ListE',
     items: [{
       title: 'items1'
     },
-      {
-        title: 'items2'
-      },
-      {
-        title: 'items3'
-      },
-      {
-        title: 'items4'
-      },
-      {
-        title: 'items5'
-      },
-      {
-        title: 'items6'
-      }]
+    {
+      title: 'items2'
+    },
+    {
+      title: 'items3'
+    },
+    {
+      title: 'items4'
+    },
+    {
+      title: 'items5'
+    },
+    {
+      title: 'items6'
+    }]
   },
   {
     headerName: 'ListE',
     items: [{
       title: 'items1'
     },
-      {
-        title: 'items2'
-      },
-      {
-        title: 'items3'
-      },
-      {
-        title: 'items4'
-      },
-      {
-        title: 'items5'
-      },
-      {
-        title: 'items6'
-      }]
+    {
+      title: 'items2'
+    },
+    {
+      title: 'items3'
+    },
+    {
+      title: 'items4'
+    },
+    {
+      title: 'items5'
+    },
+    {
+      title: 'items6'
+    }]
   },
   {
     headerName: 'ListE',
     items: [{
       title: 'items1'
     },
-      {
-        title: 'items2'
-      },
-      {
-        title: 'items3'
-      },
-      {
-        title: 'items4'
-      },
-      {
-        title: 'items5'
-      },
-      {
-        title: 'items6'
-      }]
+    {
+      title: 'items2'
+    },
+    {
+      title: 'items3'
+    },
+    {
+      title: 'items4'
+    },
+    {
+      title: 'items5'
+    },
+    {
+      title: 'items6'
+    }]
   }
 ]
